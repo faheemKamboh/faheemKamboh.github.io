7164ab0689ce85012b31d7497c3788ded198863b
Faheem Ul Islam <70851748+faheemKamboh@users.noreply.github.com>
2026-07-01 12:53:53 +0500

    Add portfolio blog route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Skills from './pages/Skills'
 import Projects from './pages/Projects'
+import Blog from './pages/Blog'
 import Contact from './pages/Contact'
 
 export default function App() {
@@ -13,11 +14,12 @@ export default function App() {
       <HashRouter>
         <Nav />
         <Routes>
-          <Route path="/"         element={<Home />} />
-          <Route path="/about"    element={<About />} />
-          <Route path="/skills"   element={<Skills />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contact"  element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </HashRouter>
     </ThemeProvider>
